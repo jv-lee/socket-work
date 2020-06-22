a59d775f5176a993cf747ffc3a449c3c008de605
jv.lee@foxmail.com <jv.lee@foxmail.com>
2020-06-22 22:59:11 +0800

    提交客户端实现

---
+++ Sokcet-Build/src/main/java/Client.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.net.Inet4Address;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+/**
+ * @author jv.lee
+ * @date 2020-06-22
+ * @description 客户端
+ */
+public class Client {
+
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket();
+        //超时时间
+        socket.setSoTimeout(3000);
+
+        //连接本地，端口2000；超时时间3000ms
+        socket.connect(new InetSocketAddress(Inet4Address.getLocalHost(), 2000), 3000);
+
+
+        System.out.println("已经发起服务器连接，并进入后续流程～");
+        System.out.println("客户端信息：" + socket.getLocalAddress() + ":" + socket.getLocalPort());
+        System.out.println("服务器信息：" + socket.getInetAddress() + ":" + socket.getPort());
+
+        try {
+            //发送接收数据
+            todo(socket);
+        } catch (Exception e) {
+            System.out.println("异常关闭:" + e.getMessage());
+        } finally {
+            //释放资源
+            socket.close();
+            System.out.println("客户端已退出~");
+        }
+
+    }
+
+    private static void todo(Socket client) throws IOException {
+        //构建键盘输入流
+        InputStream in = System.in;
+        BufferedReader input = new BufferedReader(new InputStreamReader(in));
+
+        //得到Socket输出流，并转换为打印流
+        OutputStream os = client.getOutputStream();
+        PrintStream socketPrintStream = new PrintStream(os);
+
+        //得到Socket输入流,并转换为BufferReader
+        InputStream inputStream = client.getInputStream();
+        BufferedReader socketBufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+        boolean flag = true;
+        do {
+            //键盘读取一行
+            String str = input.readLine();
+            //发送到服务器
+            socketPrintStream.println(str);
+
+            //从服务器取一行
+            String echo = socketBufferedReader.readLine();
+            if ("bye".equalsIgnoreCase(echo)) {
+                flag = false;
+            } else {
+                System.out.println(echo);
+            }
+        } while (flag);
+    }
+
+}
---
+++ Sokcet-Build/src/main/java/Server.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+/**
+ * @author jv.lee
+ * @date 2020-06-22
+ * @description 服务器端
+ */
+public class Server {
+
+    public static void main(String[] args) throws IOException {
+
+    }
+
+}
