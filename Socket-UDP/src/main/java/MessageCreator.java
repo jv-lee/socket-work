a1972f649f2423bc353eda8c91f91c031b35dc7c
jv.lee <jv.lee@foxmail.com>
2020-06-28 10:27:51 +0800

    update code

--- Socket-UDP/src/main/java/MessageCreator.java
+++ Socket-UDP/src/main/java/MessageCreator.java
@@ -7,10 +7,22 @@ public class MessageCreator {
     private static final String SN_HEADER = "收到暗号，我是（SN）:";
     private static final String PORT_HEADER = "这是暗号，请回电端口(Port):";
 
+    /**
+     * 创建固定port格式
+     *
+     * @param port
+     * @return
+     */
     public static String buildWithPort(int port) {
         return PORT_HEADER + port;
     }
 
+    /**
+     * 解析固定port格式
+     *
+     * @param data
+     * @return
+     */
     public static int parsePort(String data) {
         if (data.startsWith(PORT_HEADER)) {
             return Integer.parseInt(data.substring(PORT_HEADER.length()));
@@ -18,10 +30,22 @@ public class MessageCreator {
         return -1;
     }
 
+    /**
+     * 创建sn格式数据
+     *
+     * @param sn
+     * @return
+     */
     public static String buildWithSn(String sn) {
         return SN_HEADER + sn;
     }
 
+    /**
+     * 解析sn格式数据
+     *
+     * @param data
+     * @return
+     */
     public static String parseSn(String data) {
         if (data.startsWith(SN_HEADER)) {
             return data.substring(SN_HEADER.length());
