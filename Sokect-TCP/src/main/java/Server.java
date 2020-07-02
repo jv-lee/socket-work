e1491dec6a7bdef3120bac33d0332ea74e5f3197
jv.lee@foxmail.com <jv.lee@foxmail.com>
2020-07-02 17:49:30 +0800

    提交服务端配置设置

--- Sokect-TCP/src/main/java/Server.java
+++ Sokect-TCP/src/main/java/Server.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.net.Inet4Address;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -11,9 +13,15 @@ import java.net.Socket;
  * @description 服务器端
  */
 public class Server {
+    private static final int PORT = 20000;
 
     public static void main(String[] args) throws IOException {
-        ServerSocket server = new ServerSocket(2000);
+        ServerSocket server = createServerSocket();
+
+        initServerSocket(server);
+
+        //绑定到本地端口上
+        server.bind(new InetSocketAddress(Inet4Address.getLocalHost(), PORT), 50);
 
         System.out.println("服务器准备就绪~");
         System.out.println("服务器信息：" + server.getInetAddress() + ":" + server.getLocalPort());
@@ -25,6 +33,36 @@ public class Server {
             ClientHandler clientHandler = new ClientHandler(client);
             clientHandler.start();
         }
+    }
+
+    private static ServerSocket createServerSocket() throws IOException {
+        //创建基础的ServerSocket
+        ServerSocket serverSocket = new ServerSocket();
+
+        //绑定到本地端口20000上，并且设置当前可允许等待链接的队列为50个
+//        serverSocket = new ServerSocket(PORT);
+
+        //等价上述构造函数
+//        serverSocket = new ServerSocket(PORT, 50);
+
+        //等价上述构造函数
+//        serverSocket = new ServerSocket(PORT, 50, Inet4Address.getLocalHost());
+
+        return serverSocket;
+    }
+
+    private static void initServerSocket(ServerSocket serverSocket) throws IOException {
+        //是否复用未完全关闭的地址端口
+        serverSocket.setReuseAddress(true);
+
+        //等效Socket#setReceiveBufferSize (对accept接收到的客户端socket 设置bufferSize)
+        serverSocket.setReceiveBufferSize(64 * 1024 * 1024);
+
+        //设置serverSocket#accept超时时间 默认不设置，永久等待客户端连接
+//        serverSocket.setSoTimeout(2000);
+
+        //设置性能参数：短链接、延迟、带宽的相对重要性
+        serverSocket.setPerformancePreferences(1, 1, 1);
     }
 
     private void todo(Socket client) {
