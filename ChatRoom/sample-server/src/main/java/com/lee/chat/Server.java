6b2054b69094a7fbbeef1192bf9a03eb671ffbf2
jv.lee <jv.lee@foxmail.com>
2020-07-15 16:41:21 +0800

    修改服务器基础代码.

--- ChatRoom/sample-server/src/main/java/com/lee/chat/Server.java
+++ ChatRoom/sample-server/src/main/java/com/lee/chat/Server.java
@@ -1,7 +1,6 @@
 package com.lee.chat;
 
 
-
 import com.lee.chat.constants.TCPConstants;
 
 import java.io.BufferedReader;
@@ -9,29 +8,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Server {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         TCPServer tcpServer = new TCPServer(TCPConstants.PORT_SERVER);
         boolean isSucceed = tcpServer.start();
 
         if (!isSucceed) {
             System.out.println("Start TCP server failed!");
-        } else {
-            System.out.println("Start TCP server success!");
         }
+
         UDPProvider.start(TCPConstants.PORT_SERVER);
 
-        try {
-            //服务端读取任意输入操作 结束服务监听
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-            String str;
-            do {
-                str = bufferedReader.readLine();
-                tcpServer.broadcast(str);
-            } while (!"00bye00".equalsIgnoreCase(str));
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        //服务端读取任意输入操作 结束服务监听
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+        do {
+            str = bufferedReader.readLine();
+            tcpServer.broadcast(str);
+        } while (!"00bye00".equalsIgnoreCase(str));
 
         UDPProvider.stop();
         tcpServer.stop();
